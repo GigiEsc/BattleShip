473a6e82f702f751dc7f1d50d46d0da9c280ccfb
Gillian Pleitez <gillian_pleitez@unitec.edu>
2014-09-22 04:35:37 -0600

    Juego
    Juego de Batalla Naval en Java.

---
+++ Battle.java
@@ -0,0 +1,163 @@
+
+package battle;
+
+/**
+ *
+ * @author GigiEsc
+ */
+import java.util.Scanner; 
+public class Battle {
+   
+ public static void main(String[] args) {
+        // TODO code application logic here
+    Scanner s = new Scanner (System.in);
+
+
+//Vamos a iniciar con un pequeño titulo nuestro juego 
+//Para dar a conocer las instrucciones de el. 
+    System.out.println("Bienvenido a Batalla Naval");
+    System.out.println("Tendras que derrotar a tu oponente ingresando coordenadas");
+    System.out.println("Manos a la Obra, Capitan");
+    System.out.println("                                             "); 
+    
+ 
+ 
+   //Creacion del Tablero 
+//declaramos que tan grande sera nuestro tablero, 
+    //En este caso sera 6 x 6 
+  char tablero[][] = new char [9][9];
+       for (int i=0; i<9; i++)
+        for (int j=0; j<9; j++)
+         tablero[i][j]='_';  
+       
+       //Incluimos un pequeño ciclo que imprima las lineas que tendra nuestro tablero
+       
+         for (int i=0; i<9; i++)
+         {
+             for (int j=0; j<9; j++)
+             {
+                 System.out.print(tablero[i][j]);
+                 System.out.print(" ");
+             }
+             System.out.println(" ");
+         }
+        
+  //Aqui se creo un ciclo, las veces que se muestra el tablero 
+  //En nuestro juego, primero se declararon los tiros que el jugador posee 
+  // para terminar luego de 9 tiros 
+     int tiros = 0;
+  
+    
+ do 
+  {     
+     
+ //Aquile pediremos al usuario que ingrese las coordenadas
+ //Para iniciar el ataque al oponente.     
+ //Declaramos variables para fila y para columna y pedimos que la ingrese.   
+   int col = 0;    
+   int fila =0; 
+   tiros+=1; 
+   //Muestra el tiro actual del jugador
+   
+    System.out.println("                                             ");       
+    System.out.println("Tu tiro actual es el"+ "  "+ tiros);    
+    System.out.println("                                             "); 
+   
+    
+    System.out.println("                                             ");    
+    System.out.println("Ingrese donde quiere atacar, Capitan");
+    System.out.println("Fila:");
+    fila=s.nextInt();
+    System.out.println("Columna");
+    col=s.nextInt();
+    
+    
+    
+    
+//En esta parte, lo que deseamos hacer es por cada ataque 
+    //Que nos muestre donde se realizo y si cayo en un buque
+    //Declaramos que cuando nuestro tablero tenga fila y columna ingresada por 
+    //Usuario sea igual a un * para indicar que cayo un golpe
+ 
+    tablero[fila][col]='*';
+    
+       for (int i=0; i<9; i++)
+         {
+             for (int j=0; j<9; j++)
+             {
+                 System.out.print(tablero[i][j]);
+                 System.out.print(" ");
+             }
+             System.out.println(" ");
+         }   
+       
+         
+     //Aqui encontramos que si en algun caso nuestro usuario tira en la fila
+     //Indicada se imprimira un mensaje diciendole que su tiro esta bien
+     //Esto es solamente para indicar que los buques estan en esa area
+    //Buque 1: Ubicacion
+              
+             
+           if (fila==2 && col ==0)
+           {
+           System.out.println("                                             ");    
+           System.out.println("                                             ");    
+           System.out.print ("El disparo cayo en BUQUE!");
+           System.out.println("                                             ");    
+           System.out.println("                                             ");    
+           } 
+
+          if (fila==2 && col ==1)
+           {
+           System.out.println("                                             ");    
+           System.out.println("                                             ");    
+           System.out.print ("El disparo cayo en BUQUE!");
+           System.out.println("                                             ");    
+           System.out.println("                                             ");    
+           } 
+         
+    //Buque 2: Ubicacion       
+           if (fila==5 && col ==6)
+           {
+           System.out.println("                                             ");    
+           System.out.println("                                             ");    
+           System.out.print ("El disparo cayo en BUQUE!");
+           System.out.println("                                             ");    
+           System.out.println("                                             ");    
+           } 
+              
+           if (fila==5 && col ==7)
+           {
+           System.out.println("                                             ");    
+           System.out.println("                                             ");    
+           System.out.print ("El disparo cayo en BUQUE!");
+           System.out.println("                                             ");    
+           System.out.println("                                             "); 
+           
+           }      
+            
+           
+           
+           
+ } 
+ 
+
+ // Para que nuestro ciclo acabe, y el jugador no acierta, que termine luego de 
+ // 9 tiros, y que envie un mensaje dejandole saber que se le acabaron los tiros. 
+ 
+ while (tiros<9);
+ {
+     System.out.println ("Se te acabaron los tiros");
+     System.out.println ("Fin del Juego");
+ }  
+ 
+}      
+} 
+  
+
+            
+       
+ 
+
+
+
